5d9c8e09cf2656a6ac45ed80590cc8ee121796c3
kimnoon <za_kimnoon@hotmail.com>
2011-08-19 13:04:55 +0700

    finished version

--- NewChecker/NewChecker/ShowBroad.cs
+++ NewChecker/NewChecker/ShowBroad.cs
@@ -27,6 +27,18 @@ namespace NewChecker
         {
             Console.BackgroundColor = ConsoleColor.Yellow;
         }
+        static void yellow2()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        static void bule()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        static void red()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
         static void green()
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -38,9 +50,17 @@ namespace NewChecker
 
         public static void ShowBr(string[][] a)
         {
+            white();
+            red();
             Console.WriteLine("==============================");
+            black2();
+            bule();
             Console.WriteLine("======== Thai Ceckers ========");
+            black2();
+            red();
             Console.WriteLine("==============================");
+            black2();
+            black2();
             Console.WriteLine("");
 
             for (int i = 0; i < a.Length; i++)
@@ -87,6 +107,18 @@ namespace NewChecker
                     else if (a[i][j] == "[0]")
                     {
                         green();
+                        Console.Write(a[i][j]);
+                        black2();
+                    }
+                    else if (a[i][j] == "]H[")
+                    {
+                        green();
+                        Console.Write(a[i][j]);
+                        black2();
+                    }
+                    else if (a[i][j] == "]X[")
+                    {
+                        magenta();
                         Console.Write(a[i][j]);
                         black2();
                     }
--- NewChecker/NewChecker/Walk.cs
+++ NewChecker/NewChecker/Walk.cs
@@ -7,19 +7,36 @@ namespace NewChecker
 {
     class Walk
     {
-        public static void SimpleWalk(string[][] a,string player1,string player2)
+        static void green()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        static void magenta()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+        static void black2()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void SimpleWalk(string[][] a, string player1, string player2)
         {
             int kill = 0, turn = 0;
 
- Begin:     if (turn == 0)
+     Begin: if (turn == 0)
             {
+                magenta();
                 Console.WriteLine("player1 " + player1 + " GO!");
+                black2();
                 Console.WriteLine();
                 kill = 0;
             }
             else
             {
+                green();
                 Console.WriteLine("player2 " + player2 + " GO!");
+                black2();
                 Console.WriteLine();
                 kill = 0;
             }
@@ -34,7 +51,7 @@ namespace NewChecker
             int Rchoose = c[0];
             int Cchoose = c[1];
 
-            if (a[Rchoose][Cchoose] == "[O]" || a[Rchoose][Cchoose] == "[0]")
+            if ((a[Rchoose][Cchoose] == "[O]" || a[Rchoose][Cchoose] == "[0]") || (a[Rchoose][Cchoose] == "]X[" || a[Rchoose][Cchoose] == "]H["))
             {
             }
             else
@@ -42,9 +59,7 @@ namespace NewChecker
                 Console.WriteLine();
                 Console.Clear();
                 ShowBroad.ShowBr(a);
-                Console.WriteLine();
                 Console.WriteLine("Intput wrong please try again!!");
-                Console.WriteLine();
             }
 
             #endregion
@@ -54,32 +69,42 @@ namespace NewChecker
             if (a[Rchoose][Cchoose] == "[O]")
             {
                 turn = 1;
-
-            EatO:if ((a[Rchoose - 1][Cchoose + 1] == "[0]" && a[Rchoose - 2][Cchoose + 2] == "[ ]") 
-                    || (a[Rchoose + 1][Cchoose + 1] == "[0]" && a[Rchoose + 2][Cchoose + 2] == "[ ]"))
+            EatO:
+                if (((a[Rchoose + 1][Cchoose + 1] == "[0]" && a[Rchoose + 2][Cchoose + 2] == "[ ]") || (a[Rchoose + 1][Cchoose - 1] == "[0]" && a[Rchoose + 2][Cchoose - 2] == "[ ]")) || ((a[Rchoose + 1][Cchoose + 1] == "]H[" && a[Rchoose + 2][Cchoose + 2] == "[ ]") || (a[Rchoose + 1][Cchoose - 1] == "]H[" && a[Rchoose + 2][Cchoose - 2] == "[ ]")))
                 {
                     kill = 1;
+
                     int[] e = new int[2];
                     Console.Write("Eat  :");
                     e = Input.ex(Console.ReadLine());
                     int Erow = e[0];
                     int Ecol = e[1];
 
-                    if (((Erow == Rchoose - 1) && (Ecol == Cchoose + 1)) && a[Rchoose - 2][Cchoose + 2] == "[ ]")
-                    {
-                        a[Rchoose][Cchoose] = "[ ]";
-                        a[Erow][Ecol] = "[ ]";
-                        a[Rchoose - 2][Cchoose + 2] = "[O]";
-                        Rchoose = Rchoose - 2;
-                        Cchoose = Cchoose - 2;
-                    }
-                    else if (((Erow == Rchoose + 1) && (Ecol == Cchoose + 1)) && a[Rchoose + 2][Cchoose + 2] == "[ ]")
+                    if (((Erow == Rchoose + 1) && (Ecol == Cchoose + 1)) && a[Rchoose + 2][Cchoose + 2] == "[ ]")
                     {
                         a[Rchoose][Cchoose] = "[ ]";
                         a[Erow][Ecol] = "[ ]";
                         a[Rchoose + 2][Cchoose + 2] = "[O]";
                         Rchoose = Rchoose + 2;
+                        Cchoose = Cchoose + 2;
+                        if (Rchoose == 8)
+                        {
+                            a[Rchoose][Cchoose] = "]X[";
+
+                        }
+                    }
+                    else if (((Erow == Rchoose + 1) && (Ecol == Cchoose - 1)) && a[Rchoose + 2][Cchoose - 2] == "[ ]")
+                    {
+                        a[Rchoose][Cchoose] = "[ ]";
+                        a[Erow][Ecol] = "[ ]";
+                        a[Rchoose + 2][Cchoose - 2] = "[O]";
+                        Rchoose = Rchoose + 2;
                         Cchoose = Cchoose - 2;
+                        if (Rchoose == 8)
+                        {
+                            a[Rchoose][Cchoose] = "]X[";
+
+                        }
                     }
 
                     Console.WriteLine();
@@ -88,7 +113,7 @@ namespace NewChecker
 
                     goto EatO;
                 }
-                else if(kill == 0)
+                else if (kill == 0)
                 {
                     int[] g = new int[2];
                     Console.Write("Enter go : ");
@@ -99,24 +124,29 @@ namespace NewChecker
                         || ((Rgo == Rchoose + 1) && (Cgo == Cchoose + 1))
                         && a[Rgo][Cgo] == "[ ]")
                     {
-                        a[Rgo][Cgo] = "[O]";
-                        a[Rchoose][Cchoose] = "[ ]";
+                        if (Rgo == 8)
+                        {
+                            a[Rgo][Cgo] = "]X[";
+                            a[Rchoose][Cchoose] = "[ ]";
+                        }
+                        else
+                        {
+                            a[Rgo][Cgo] = "[O]";
+                            a[Rchoose][Cchoose] = "[ ]";
+                        }
                     }
                     else
                     {
                         Console.WriteLine();
                         Console.Clear();
                         ShowBroad.ShowBr(a);
-                        Console.WriteLine();
                         Console.WriteLine("Intput wrong please try again!!");
-                        Console.WriteLine();
                     }
 
-                    Console.WriteLine();
-                    Console.Clear();
-                    ShowBroad.ShowBr(a);
                 }
-            goto Begin;
+                Console.Clear();
+                ShowBroad.ShowBr(a);
+                goto Begin;
             }
 
             #endregion
@@ -126,9 +156,8 @@ namespace NewChecker
             else if (a[Rchoose][Cchoose] == "[0]")
             {
                 turn = 0;
-
-            Eat0:if ((a[Rchoose - 1][Cchoose - 1] == "[O]" && a[Rchoose - 2][Cchoose - 2] == "[ ]") 
-                    || (a[Rchoose - 1][Cchoose + 1] == "[O]" && a[Rchoose - 2][Cchoose + 2] == "[ ]"))
+            Eat0:
+                if (((a[Rchoose - 1][Cchoose - 1] == "[O]" && a[Rchoose - 2][Cchoose - 2] == "[ ]") || (a[Rchoose - 1][Cchoose + 1] == "[O]" && a[Rchoose - 2][Cchoose + 2] == "[ ]")) || ((a[Rchoose - 1][Cchoose - 1] == "]X[" && a[Rchoose - 2][Cchoose - 2] == "[ ]") || (a[Rchoose - 1][Cchoose + 1] == "]X[" && a[Rchoose - 2][Cchoose + 2] == "[ ]")))
                 {
                     kill = 1;
 
@@ -145,6 +174,11 @@ namespace NewChecker
                         a[Rchoose - 2][Cchoose - 2] = "[0]";
                         Rchoose = Rchoose - 2;
                         Cchoose = Cchoose - 2;
+                        if (Rchoose == 1)
+                        {
+                            a[Rchoose][Cchoose] = "]H[";
+
+                        }
                     }
                     else if (((Erow == Rchoose - 1) && (Ecol == Cchoose + 1)) && a[Rchoose - 2][Cchoose + 2] == "[ ]")
                     {
@@ -153,6 +187,11 @@ namespace NewChecker
                         a[Rchoose - 2][Cchoose + 2] = "[0]";
                         Rchoose = Rchoose - 2;
                         Cchoose = Cchoose + 2;
+                        if (Rchoose == 1)
+                        {
+                            a[Rchoose][Cchoose] = "]H[";
+
+                        }
                     }
 
                     Console.WriteLine();
@@ -161,7 +200,7 @@ namespace NewChecker
 
                     goto Eat0;
                 }
-                else if(kill == 0)
+                else
                 {
                     int[] g = new int[2];
                     Console.Write("Enter go : ");
@@ -172,25 +211,191 @@ namespace NewChecker
                         || ((Rgo == Rchoose - 1) && (Cgo == Cchoose + 1))
                         && a[Rgo][Cgo] == "[ ]")
                     {
-                        a[Rgo][Cgo] = "[0]";
-                        a[Rchoose][Cchoose] = "[ ]";
+                        if (Rgo == 1)
+                        {
+                            a[Rgo][Cgo] = "]H[";
+                            a[Rchoose][Cchoose] = "[ ]";
+                        }
+                        else
+                        {
+                            a[Rgo][Cgo] = "[0]";
+                            a[Rchoose][Cchoose] = "[ ]";
+                        }
                     }
                     else
                     {
                         Console.WriteLine();
                         Console.Clear();
                         ShowBroad.ShowBr(a);
-                        Console.WriteLine();
                         Console.WriteLine("Intput wrong please try again!!");
-                        Console.WriteLine();
                     }
 
+                }
+                Console.Clear();
+                ShowBroad.ShowBr(a);
+                goto Begin;
+            }
+            #endregion
 
+            #region horse O
+            else if (a[Rchoose][Cchoose] == "]X[")
+            {
+                turn = 1;
+            Eathorse0: int[] e = new int[2];
+                Console.Write("Hot GO :");
+                e = Input.ex(Console.ReadLine());
+                int Erow = e[0];
+                int Ecol = e[1];
+
+                
+                if (((Erow < Rchoose) && (Ecol < Cchoose)) && (a[Erow - 1][Ecol - 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow - 1][Ecol - 1] = "]X[";
+                    Rchoose = Ecol - 1;
+                    Cchoose = Erow - 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow < Rchoose) && (Ecol > Cchoose)) && (a[Erow - 1][Ecol + 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow - 1][Ecol + 1] = "]X[";
+                    Rchoose = Ecol - 1;
+                    Cchoose = Erow + 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow > Rchoose) && (Ecol < Cchoose)) && (a[Erow + 1][Ecol - 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow + 1][Ecol - 1] = "]X[";
+                    Rchoose = Ecol + 1;
+                    Cchoose = Erow - 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow > Rchoose) && (Ecol > Cchoose)) && (a[Erow + 1][Ecol + 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow + 1][Ecol + 1] = "]X[";
+                    Rchoose = Ecol + 1;
+                    Cchoose = Erow + 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if ((kill == 0)&&(a[Erow][Ecol] == "[ ]"))
+                {
+                    a[Erow][Ecol] = "]X[";
+                    a[Rchoose][Cchoose] = "[ ]";
+                }
+                else 
+                
+                {
                     Console.WriteLine();
                     Console.Clear();
                     ShowBroad.ShowBr(a);
+                    Console.WriteLine("Intput wrong please try again!!");
                 }
-            goto Begin;
+
+                Console.WriteLine();
+                Console.Clear();
+                ShowBroad.ShowBr(a);
+                goto Begin;
+            }
+
+            #endregion
+
+            #region horse 0
+            else if (a[Rchoose][Cchoose] == "]H[")
+            {
+                turn = 0;
+            Eathorse0:
+                int[] e = new int[2];
+                Console.Write("Hot GO :");
+                e = Input.ex(Console.ReadLine());
+                int Erow = e[0];
+                int Ecol = e[1];
+
+                
+                if (((Erow < Rchoose) && (Ecol < Cchoose)) && (a[Erow - 1][Ecol - 1] == "[ ]"))
+                {
+                    kill = 1 ;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow - 1][Ecol - 1] = "]H[";
+                    Rchoose = Ecol - 1;
+                    Cchoose = Erow - 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow < Rchoose) && (Ecol > Cchoose)) && (a[Erow - 1][Ecol + 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow - 1][Ecol + 1] = "]H[";
+                    Rchoose = Ecol - 1;
+                    Cchoose = Erow + 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow > Rchoose) && (Ecol < Cchoose)) && (a[Erow + 1][Ecol - 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow + 1][Ecol - 1] = "]H[";
+                    Rchoose = Ecol + 1;
+                    Cchoose = Erow - 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if (((Erow > Rchoose) && (Ecol > Cchoose)) && (a[Erow + 1][Ecol + 1] == "[ ]"))
+                {
+                    kill = 1;
+                    a[Rchoose][Cchoose] = "[ ]";
+                    a[Erow][Ecol] = "[ ]";
+                    a[Erow + 1][Ecol + 1] = "]H[";
+                    Rchoose = Ecol + 1;
+                    Cchoose = Erow + 1;
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    goto Eathorse0;
+                }
+                else if ((kill == 0) && (a[Erow][Ecol] == "[ ]"))
+                {
+                    a[Erow][Ecol] = "]H[";
+                    a[Rchoose][Cchoose] = "[ ]";
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Clear();
+                    ShowBroad.ShowBr(a);
+                    Console.WriteLine("Intput wrong please try again!!");
+                }
+
+                Console.WriteLine();
+                Console.Clear();
+                ShowBroad.ShowBr(a);
+
+                goto Begin;
             }
 
             #endregion
